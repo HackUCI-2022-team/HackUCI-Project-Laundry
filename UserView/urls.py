c3584896ad29505a912ff92e99f1d4977c9c9e54
steveck <steveck@uci.edu>
2022-02-26 14:53:56 -0800

    registration

--- UserView/urls.py
+++ UserView/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
+    path('', views.home_page, name='home'),
+    path('signup', views.signup_page, name='signup'),
+    path('login', views.login_page, name='login')
 ]
