92856a6c628fae50091b2b5dde450085aec01926
steveck <steveck@uci.edu>
2022-02-26 12:59:26 -0800

    migrate models

--- UserView/admin.py
+++ UserView/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Laundry, Dryer, Notification
+from .models import Laundry, Dryer, Notification, Floor
 
 # Register your models here.
 admin.site.register(Laundry)
 admin.site.register(Dryer)
 admin.site.register(Notification)
+admin.site.register(Floor)
